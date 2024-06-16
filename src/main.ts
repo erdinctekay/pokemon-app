f1928306099ba0e852e609f4578b2f7922fe20b6
Erdinc Tekay <erdinctekay@gmail.com>
2024-06-17 01:17:32 +0300

    chore: added tailwind, vuetify, md icons and updated base styles

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,31 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
-import App from './App.vue'
 import router from './router'
 
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import '@mdi/font/css/materialdesignicons.css'
+
+import App from './App.vue'
 const app = createApp(App)
 
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi
+    }
+  }
+})
+
+app.use(vuetify)
 app.use(createPinia())
 app.use(router)
 
