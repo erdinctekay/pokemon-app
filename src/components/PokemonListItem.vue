d2b0453c146e29efa5ac47c8b21ec58e504d51f2
Erdinc Tekay <erdinctekay@gmail.com>
2024-06-18 01:01:45 +0300

    added PokemonListItem component and home view to list view

---
+++ src/components/PokemonListItem.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-col cols="12" sm="6" md="4" lg="3">
+    <v-card
+      variant="tonal"
+      style="min-height: var(--card-height)"
+      class="pokemon-list-item py-5"
+      @click="pokemonStore.goPokemonDetail(pokemon)"
+    >
+      <div class="flex items-center justify-center" :style="{ 'min-height': imgWidth + 'px' }">
+        <v-img
+          v-if="pokemon.image !== undefined"
+          :src="pokemon.image || `https://via.placeholder.com/${imgWidth}`"
+          class="mx-auto select-none pointer-events-none"
+          aspect-ratio="1"
+          :height="imgWidth"
+        />
+        <v-progress-circular v-else indeterminate color="primary" size="48" />
+      </div>
+      <v-card-title class="select-none"
+        >{{
+          pokemon.name
+            .split('-')
+            .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+            .join(' ')
+        }}
+      </v-card-title>
+    </v-card>
+  </v-col>
+</template>
+<script setup lang="ts">
+import { usePokemonStore } from '@/stores/pokemon'
+import type { Pokemon } from '@/types/pokemon'
+
+const imgWidth = 135
+const pokemonStore = usePokemonStore()
+
+defineProps<{ pokemon: Pokemon }>()
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,80 @@
 <template>
-  <div class="flex flex-row items-center justify-center" style="height: calc(100dvh - 64px)">
-    <div class="flex flex-col items-center">
-      <h1 class="text-5xl font-bold">HOME</h1>
+  <div class="flex flex-row items-start justify-center" style="min-height: calc(100dvh - 64px)">
+    <div class="flex flex-col py-5 container">
+      <v-row class="content-start" style="min-height: 750px; --card-height: 225px">
+        <template v-if="pokemonStore.loading">
+          <v-col v-for="n in pokemonStore.take" :key="n" cols="12" sm="6" md="4" lg="3">
+            <v-skeleton-loader style="height: var(--card-height)" />
+          </v-col>
+        </template>
+        <pokemon-list-item
+          v-else
+          v-for="pokemon in pokemonStore.pokemons"
+          :key="pokemon.name"
+          :pokemon="pokemon as Pokemon"
+        />
+      </v-row>
+
+      <v-pagination
+        class="mt-5"
+        v-model="pokemonStore.page"
+        :length="Math.ceil(pokemonStore.total / pokemonStore.take)"
+        @input="pokemonStore.setPage"
+        :total-visible="5"
+      />
+
+      <div style="position: absolute; top: 100px; right: 0; left: 0">
+        <v-alert
+          v-for="error in pokemonStore.errors"
+          :key="error"
+          type="error"
+          variant="tonal"
+          style="max-width: 360px; margin: 0.5rem auto"
+        >
+          {{ error }}
+        </v-alert>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-//
+import { onBeforeMount, onBeforeUnmount, watch } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+import { usePokemonStore } from '@/stores/pokemon'
+
+import type { Pokemon } from '@/types/pokemon'
+import PokemonListItem from '@/components/PokemonListItem.vue'
+
+const router = useRouter()
+const route = useRoute()
+
+const pokemonStore = usePokemonStore()
+
+onBeforeMount(() => {
+  const page = route.query.page
+  page ? pokemonStore.setPage(Number(page)) : router.push({ query: { page: pokemonStore.page } })
+})
+
+onBeforeUnmount(() => {
+  pokemonStore.clearErrors()
+})
+
+// watch page and trigger route
+watch(
+  () => pokemonStore.page,
+  (page) => router.push({ query: { page } })
+)
+
+// watch route and trigger list
+watch(
+  () => route.query.page,
+  (page) => {
+    if (page) {
+      if (page !== String(pokemonStore.page)) pokemonStore.setPage(Number(page))
+      pokemonStore.listPokemons()
+    }
+  },
+  { immediate: true }
+)
 </script>
